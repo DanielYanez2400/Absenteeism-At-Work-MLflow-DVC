d1f8600f6e3eab0e437164855ed3a1ee001e4416
Daniel Yanez <danielyaneztorres5@gmail.com>
2024-06-01 18:56:53 -0600

    Data Transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.Classifier import logger
 from src.Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.Classifier.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.Classifier.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -18,6 +19,17 @@ STAGE_NAME = 'Data Validation stage'
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Data Transformation stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
